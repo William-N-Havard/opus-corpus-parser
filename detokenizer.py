4485684834187d6dcb1c62d14a2cdd288257228d
William N. Havard <william.havard@gmail.com>
2018-03-12 16:34:30 +0100

    Update detokenizer.py

--- detokenizer.py
+++ detokenizer.py
@@ -13,24 +13,32 @@ def en(token_list):
     text = ' '.join(token_list)
 
     remove_double_spaces = re.compile(r' +')
-    remove_space_before_punct = re.compile(r" ([,.:;?!'])")
+    remove_space_before_punct = re.compile(r' ([,.:;?!\'`/\(\)\[\]])')
+    remove_space_after_punct = re.compile(r'([\'`/\(\)\[\]]) ')
+    remove_inner_spaces = re.compile(r'" (.+?) "')
 
-    text = text.strip()
-    text = text.replace('`', '\'')
-    text = text.replace(' \'','\'')
-    text = text.replace('\' ','\'')    
-    text = text.replace(' "', '"')
-    text = text.replace('" ', '"')
+    text = text.strip()   
+    text = text.replace('`', '\'') 
     text = remove_double_spaces.sub(' ', text)
     text = remove_space_before_punct.sub(r'\1', text)
+    text = remove_space_after_punct.sub(r'\1', text)
+    text = remove_inner_spaces.sub(r'"\1"', text) 
     return text
+
+def rm_commentary(sentence):
+    begin_=['<', '[', '(']
+    end_=['>', ']', ')']
+    if len(sentence)!=0 and sentence[0] not in begin_ and sentence[-1] not in end_:
+        return sentence
+    else:
+        return None
 
 def tokens_as_str(token_list):
     return ' '.join(token_list)
 
 def transform(sentences_as_token_list, postprocess):
     for process in postprocess.split(','):
+        process = process.strip()
         for i, sentence in enumerate(sentences_as_token_list):
-            process = process.strip()
             sentences_as_token_list[i]=globals()[process](sentences_as_token_list[i])
-    return sentences_as_token_list
+    return [x for x in sentences_as_token_list if x is not None]
